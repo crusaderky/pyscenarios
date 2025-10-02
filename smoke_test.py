cba3321b2690c9a016e859178ea47649ad792fd7
Guido Imperiale <crusaderky@gmail.com>
2025-10-02 23:49:00 +0200

    Reinstate MacIntel CI (#50)

--- smoke_test.py
+++ smoke_test.py
@@ -1,10 +1,12 @@
 """Test import the library and print essential information"""
 
+import platform
 import sys
 
 import pyscenarios
 
 print("Python interpreter:", sys.executable)
 print("Python version    :", sys.version)
+print("Platform          :", platform.platform())
 print("Library path      :", pyscenarios.__file__)
 print("Library version   :", pyscenarios.__version__)
